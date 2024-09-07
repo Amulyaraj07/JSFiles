edc811f9d3ca82a27c03090bfc8007fbd7a0764c
Amulya Raj <amulyaraj1234@gmail.com>
2024-09-07 10:47:40 +0000

    Operations on datatypes

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -33,5 +33,47 @@ let stringNum=String(num);
 console.log(typeof stringNum); //string
 
 
+//************Operations****************//
+
+let value=3;
+let negVal=-value;
+console.log(negVal); //-3
+
+console.log(2 + 2); // Addition
+console.log(4 - 2); // Subtraction
+console.log(2 * 2); // Multiplication
+console.log(4 / 2); // Division
+console.log(5 % 2); // Modulus (remainder)
+console.log(2 ** 3); // Exponentiation (2 raised to the power of 3)
+
+//operation on string
+
+let str1="amulya";
+let str2="raj";
+let str3=str1+str2;
+console.log(str3);
+
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log("1"+2+2); //122
+console.log(1+2+"2")//32
+
+console.log(+true)//1
+console.log(+"");//0
+
+
+ let num1,num2,num3;
+
+//  num1=num2=num3=2+2; //not readable
+
+let count=100;
+count++;//post increment
+console.log(count); //101
+++count;//pre increment
+console.log(count);//102
+
+
+
+
 
 
