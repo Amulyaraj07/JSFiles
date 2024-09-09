ad3c394339a80e79c14f412a554a73d932384311
Amulya Raj <amulyaraj1234@gmail.com>
2024-09-09 17:53:41 +0000

    functions

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -27,3 +27,36 @@ function loginUserMessage(username){
 
 console.log(loginUserMessage("Amulya"));
 console.log(loginUserMessage());//undefined
+
+
+function calculateCartPrice(...num1){
+    return num1;
+}
+
+console.log(calculateCartPrice(200));
+
+console.log(calculateCartPrice(200,400,500));//use REST operator(...)
+
+const user = {
+    username: "amulya",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "raj",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
