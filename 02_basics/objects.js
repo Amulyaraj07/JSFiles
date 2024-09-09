a791f6731a4a778bfcbc2c648f4dc85f0fe7da30
Amulya Raj <amulyaraj1234@gmail.com>
2024-09-09 17:01:31 +0000

    objects

--- 02_basics/objects.js
+++ 02_basics/objects.js
@@ -1,2 +1,41 @@
 //Objects:
+//singleton: constructor se banega to singleton
+Object.create();
 
+//if we want to use a symbol in objects the we should use []
+const mySym = Symbol("key1")
+//object literals
+ 
+const JsUser={
+    name:"Amulya",
+    age:18,
+    email:"amulya@gmail.com",
+    isActive:false,
+    lastActiveDays:["Mon","Tue"]
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+
+
+
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "amulya@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "amulya@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
