9c05d2e7fb984048073f967a9c4abe698df3e155
Amulya Raj <amulyaraj1234@gmail.com>
2024-09-09 17:39:36 +0000

    updated functions

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -11,3 +11,12 @@ function add(num1,num2){ //parameters
 
 add(4,5);//arguments
 
+const res=add(4,5);
+console.log(res); //undefined because the function is printing not returning 
+
+function mul(num1,num2){
+    return num1*num2;
+}
+
+const result=mul(4,5);
+console.log(result);
