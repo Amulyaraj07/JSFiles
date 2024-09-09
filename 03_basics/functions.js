85f70258fe808b852d9dc4ece51bc898032aab30
Amulya Raj <amulyaraj1234@gmail.com>
2024-09-09 17:35:28 +0000

    functions

---
+++ 03_basics/functions.js
@@ -0,0 +1,13 @@
+function sayMyname(){
+    console.log("Amulya");
+}
+
+sayMyname();
+sayMyname();
+
+function add(num1,num2){ //parameters
+    console.log(num1+num2);
+}
+
+add(4,5);//arguments
+
