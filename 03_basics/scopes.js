8ee4e3629a5a10e664b0b340a04ccea269dcb9c4
Amulya Raj <amulyaraj1234@gmail.com>
2024-09-10 09:22:30 +0000

    scopes

--- 03_basics/scopes.js
+++ 03_basics/scopes.js
@@ -14,3 +14,45 @@ if(true){
 console.log(a);//error
 console.log(b);//error
 console.log(c);//print without error
+
+function one(){
+    const username="amulya";
+    function two(){
+        const website="youtube";
+        console.log(username);
+    }
+    console.log(website);//error
+    two();
+}
+
+one();
+two(); //error
+
+if(true){
+    const username="amulya";
+    if(username==="amulya"){
+        const website="youtube"
+        console.log(username+website);
+    }
+    console.log(website);//error
+}
+
+console.log(username);//error
+
+///////////////////////////////////////////////////
+addOne(6);//also works fine before declaration 
+function addOne(num){
+    return num+1;
+}
+
+addOne(6);//works fine
+
+
+addTwo(5);//error because we have stored it in some variable ,here comes the concept of hoisting
+const addTwo=function(num){
+    return num+2;
+}
+
+addTwo(5);//works fine
+
+
