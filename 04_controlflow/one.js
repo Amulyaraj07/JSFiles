464968cddd430afded76e6354dea010cfe4d2fd6
Amulya Raj <amulyaraj1234@gmail.com>
2024-09-14 09:16:41 +0000

    added file

---
+++ 04_controlflow/one.js
@@ -0,0 +1,18 @@
+if (3>2){
+    console.log("True");
+}else if(3>5){
+    console.log("false");
+}else{
+    console.log("false");
+}
+
+// <,>,<=,>=,==, != , ===,!==
+
+
+switch (num){
+    case 1:
+        console.log("one");
+        break;
+    default:
+        console.log("default")
+}
